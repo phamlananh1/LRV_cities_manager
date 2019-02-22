eebd76eb03a4e34644d94e9313a1a0539c048cc2
tranminhhieu90 <hieuminhtm90@gmail.com>
2019-02-22 08:10:22 +0700

    update ngay 22/2/2019

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::group(['prefix'=>'customers'],function (){
     Route::post('/create','CustomerController@store')->name('customers.store');
     Route::get('filter','CustomerController@filterByCity')->name('customers.filterByCity');
     Route::get('/{id}/edit','CustomerController@edit')->name('customers.edit');
-    Route::post('/{id}.edit','CustomerController@update')->name('customers.update');
-    Route::get('/destroy','CustomerController@destroy')->name('customers.destroy');
+    Route::post('/{id}/edit','CustomerController@update')->name('customers.update');
+    Route::get('/{id}/destroy','CustomerController@destroy')->name('customers.destroy');
 
 });
